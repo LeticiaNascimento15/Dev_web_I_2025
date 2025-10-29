c9b9600aea75b026d986ae952d4d75315c94cd8d
LeticiaNascimento15 <165960605+LeticiaNascimento15@users.noreply.github.com>
2025-10-29 19:27:39 -0300

    Update class_pai.class.php

--- objeto/model/class_pai.class.php
+++ objeto/model/class_pai.class.php
@@ -1,9 +1,10 @@
 <?php
-
-    abstract class ClassePai {
+ 
+       abstract class ClassePai {
         public $id;
         private $nomeArquivo="";
         const SEPARADOR = "#";
+        const NOME_ARQUIVO = "";
         public function __construct($id, $nomeArquivo) {
             $this->id = $id;
             $this->nomeArquivo = $nomeArquivo;
@@ -11,17 +12,19 @@
         abstract function montaLinhaDados();
 
         public function encontraUltimoId(){
-            $arquivo = fopen($this->nomeArquivo, "r");
-            $idTemp = 1;
-            while(!feof($arquivo)){
-                $linha = fgets($arquivo);
+         $arquivo = fopen($this->nomeArquivo, "r");
+         $idTemp = 1;
+         while(!feof($arquivo)){
+            $linha = fgets($arquivo);
                 if(empty($linha))
-                    continue;
+                continue;
                 $dados = explode(self::SEPARADOR, $linha);
-                $idTemp = intval($dados[0])+1;
+             $idTemp = intval($dados[0])+1;
             }
             $this->id=$idTemp;
+            fclose($arquivo);
         }
+         
         public function cadastrar() {
             $this->encontraUltimoId();
             //TODO: Cadastrar  no arquivo.
@@ -30,11 +33,54 @@
             fclose($arquivo);
         }
         public function remover() {
-            //TODO: Remover funcionário do arquivo.
+            $arquivo = fopen($this->nomeArquivo, "r+");
+            $auxiliar = "";
+            while(!feof($arquivo)){
+                $linha = fgets($arquivo);
+                var_dump($linha);
+              if(empty($linha))
+                    continue;
+              $dados = explode(self::SEPARADOR, $linha);
+                
+              if($dados[0] != $this->id){
+                  $auxiliar .= $linha; 
+                }
+            }
+            if(!empty($auxiliar)){
+              ftruncate($arquivo, 0);
+              rewind($arquivo);
+              fwrite($arquivo, $auxiliar);
+            }
+            
+            fclose($arquivo);
+         
         }
         public function alterar() {
-            //TODO: Alterar linha do funcionário funcionário no arquivo.
+     $arquivo = fopen($this->nomeArquivo, "r+");
+     $auxiliar = "";
+      while(!feof($arquivo)){
+                $linha = fgets($arquivo);
+          var_dump($linha);
+         if(empty($linha)) 
+                    continue;
+                $dados = explode(self::SEPARADOR, $linha);
+
+                
+          if($dados[0] == $this->id){
+                    $auxiliar .= $this->montaLinhaDados()."\n";
+              }
+          else {
+                  $auxiliar .= $linha;
+                }
+            }
+         if(!empty($auxiliar)){
+                ftruncate($arquivo, 0);
+                rewind($arquivo);
+                fwrite($arquivo, $auxiliar);
+            }
+               
+            fclose($arquivo);
         }
     }
 
-?>
+?> 
