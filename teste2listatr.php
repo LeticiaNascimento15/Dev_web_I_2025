f2e88ba4a44a078426780f58478a32e5c5d22224
LeticiaNascimento15 <165960605+LeticiaNascimento15@users.noreply.github.com>
2026-06-11 15:43:29 -0300

    Criar teste2listatr.php

---
+++ teste2listatr.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+require_once "../../util/conexaoI2.php";
+
+try {
+
+    $pdo = getPDO();
+
+    $sql = "SELECT * FROM funcionario";
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+
+    $funcionarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    header('Content-Type: application/json; charset=utf-8');
+
+    echo json_encode($funcionarios, JSON_UNESCAPED_UNICODE);
+
+} catch (Throwable $e) {
+
+    http_response_code(500);
+
+    echo json_encode([
+        "erro" => $e->getMessage()
+    ]);
+}
