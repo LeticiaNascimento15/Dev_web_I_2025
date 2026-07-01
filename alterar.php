89b8160c2ec2eb185d1ba0f2a1f5b3b50451aae3
LeticiaNascimento15 <165960605+LeticiaNascimento15@users.noreply.github.com>
2026-07-01 16:58:16 -0300

    Add files via upload

--- alterar.php
+++ alterar.php
@@ -1,6 +1,6 @@
 <?php
 declare(strict_types=1);
-//require_once '../model/funcoes.php';
+require_once '../model/funcoes.php';
 require_once '../model/funcoesBD.php';
 require_once '../../util/funcoes.php';
 
